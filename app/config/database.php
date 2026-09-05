23824ec07c07e81bf040fce45181b43815520bf0
Dana Romualdo <danaromualdo09@gmail.com>
2026-09-05 20:25:26 +0800

    Docker and Dbase

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> getenv('DB_DRIVER') ?: 'mysql',
-    'hostname'	=> getenv('DB_HOST') ?: 'localhost',
-    'port'		=> getenv('DB_PORT') ?: '3306',
-    'username'	=> getenv('DB_USER') ?: 'root',
+    'hostname'	=> getenv('DB_HOST') ?: '',
+    'port'		=> getenv('DB_PORT') ?: '',
+    'username'	=> getenv('DB_USER') ?: 'avnadmin',
     'password'	=> getenv('DB_PASSWORD') ?: '',
-    'database'	=> getenv('DB_NAME') ?: 'mydb',
+    'database'	=> getenv('DB_NAME') ?: '',
     'charset'	=> getenv('DB_CHARSET') ?: 'utf8mb4',
     'dbprefix'	=> getenv('DB_PREFIX') ?: '',
     // Optional for SQLite
